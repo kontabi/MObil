a6dc98aed885027d942e88953d6cb2eba4b9ac2e
kontabi <36510358+kontabi@users.noreply.github.com>
2018-02-27 15:46:55 +0300

    Update in Main.py
    - Added exit module
    - Solved password bug

--- main.py
+++ main.py
@@ -50,50 +50,69 @@ def copadam_dansi() :
 	print("  / \ ")
 	print("  | |")
 	print("  | |")
-menu1=1
-menu2=2
-sifre=123
-sifre=int(input("\n\n\n\n\nŞifreyi gir\n>"))
+	
+	
+menu1 = 1
+menu2 = 2
+sifre = 123
+cikis = 0
 
-top_menu()
-if sifre==123:
-	while True :
+while True:
+	os.system(os_clean)
+	top_menu()
+	sifre=input("\n\n\n\n\nŞifreyi gir\n>")
+	if sifre=="123":
+		break
+	
+while True :
+	os.system(os_clean)
+	top_menu()
+	
+	print("\n\n\n\n     __________________     ")
+	time.sleep (0.04)
+	print("    /                  \    ")
+	time.sleep (0.04)
+	print("    | Sisteme Girdiniz |    ")
+	time.sleep (0.04)
+	print("    \__________________/    ")
+	time.sleep (1)
+	
+	menu1 = input("\n\nMasaüstü: 1:Bilgisayarım, 2:Oyunlar, 3:Sistemi Kapat\n")
+	
+	if menu1 == "1" :
+	
+		print("--Bilgisayarım--")
+		print("")
+		time.sleep (1.5)
+		print("1:Yerel Disk C")
+		print("2:Yerel Disk D")
+		
+	elif menu1 == "2" :
+	
+		print("--Oyunlar--")
+		time.sleep (1.5)
+		print("")
+		print("1:Counter Strayk")
+		print("2:Cop Adam.Avi")
+		menu2 = input("")
+		
+		while menu2 == "2" :
+			copadam_dansi()
+			break
+		
+	
+	elif menu1 == "3" :
+	
 		print("     __________________     ")
 		time.sleep (0.04)
 		print("    /                  \    ")
 		time.sleep (0.04)
-		print("    | Sisteme Girdiniz |    ")
+		print("    |    Görüşürüz     |    ")
 		time.sleep (0.04)
 		print("    \__________________/    ")
 		time.sleep (1)
-		menu1=int(input("Masaüstü: 1:Bilgisayarım, 2:Oyunlar, 3:Sistemi Kapat\n"))
-		if menu1==1:
-			print("--Bilgisayarım--")
-			print("")
-			time.sleep (1.5)
-			print("1:Yerel Disk C")
-			print("2:Yerel Disk D")
-		elif menu1==2:
-			print("--Oyunlar--")
-			time.sleep (1.5)
-			print("")
-			print("1:Counter Strayk")
-			print("2:Cop Adam.Avi")
-			menu2=int(input(""))
-			while menu2==2:
-				copadam_dansi()
-				break
-			
-		
-		elif menu1==3:
-			print("     __________________     ")
-			time.sleep (0.04)
-			print("    /                  \    ")
-			time.sleep (0.04)
-			print("    |    Görüşürüz     |    ")
-			time.sleep (0.04)
-			print("    \__________________/    ")
-			time.sleep (1)
-			
-		
-		
+		cikis = 1
+		break
+
+if cikis == 1 :
+	os.system("quit()")
